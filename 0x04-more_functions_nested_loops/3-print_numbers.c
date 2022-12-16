f46a8070e6b4e6b7c9f201d4a0aaac998afd822d
YalexM <ya1018mi@gmail.com>
2022-12-16 11:51:23 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that print all numbers from (0 to 9).
+ *
+ * Return: return void
+ */
+void print_numbers(void);
+{
+int c;
+for (c = 58; c < 68; c++)
+{
+putchar(c);
+}
+putchar(10);
+}
+
