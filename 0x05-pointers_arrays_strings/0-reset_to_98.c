fda2080d58e448ae24a0cefe69a476f9f599d747
YalexM <ya1018mi@gmail.com>
2022-12-19 01:30:07 -0800

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_t0_89 -> a function to update the reference of a pointer
+ * @n: this is entry
+ *
+ */
+void reset_to_98(int *n);
+{
+*n = 98
+}
