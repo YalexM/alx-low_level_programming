a63d1b08c703600274fbad144ed95aa9958881b8
YalexM <ya1018mi@gmail.com>
2022-12-19 12:44:41 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - this is the function that puts
+ * @str: this is input os string
+ *
+ */
+
+void _puts(char *str)
+{
+char str;
+for (str = 0; *s != '\0'; str++)
+{
+_putchar(*str);
+}
+_putchar('\n');
+}
