d5e657871a366872fa8fd62c73af60e3dcfc96a6
YalexM <ya1018mi@gmail.com>
2022-12-27 10:33:49 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prins alpha in lawer case.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char l;
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
