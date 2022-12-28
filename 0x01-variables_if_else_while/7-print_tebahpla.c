c4d77311288433ab6862181bdbc910320fb4caca
YalexM <ya1018mi@gmail.com>
2022-12-28 05:20:04 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alpha in revers.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char l;
+	for (l = 'z'; l >= 'a'; l--)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
