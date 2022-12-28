f7363dbd99fcc194cf17425975e3458a4cf673e5
YalexM <ya1018mi@gmail.com>
2022-12-28 05:42:08 -0800

    task 7

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number in base 16.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num;
+char l;
+	for (n = 0; n < 10; num++)
+	putchar((num % 10) + '0');
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
