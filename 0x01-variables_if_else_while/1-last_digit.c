1925abc5b1652f45163aee6bfee4c37f5c3cf633
YalexM <ya1018mi@gmail.com>
2022-12-30 05:14:10 -0800

    tas 1 fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ printf("Last digit of  %d98 is %d8  and is great than 5\n", n, i);
 }
 else if (i == 0)
 {
-printf("Last digit of %d is  %d and is 0\n", n, i);
+printf("Last digit of %d -98 is  %d -8 and is 0\n", n, i);
 }
 else
 {
-printf("Last digit of %d is  %d and is less than 6 and not 0\n", n, i);
+printf("Last digit of %d 980 is  %d 0 and is less than 6 and not 0\n", n, i);
 }
 /* your code goes there */
 return (0);
