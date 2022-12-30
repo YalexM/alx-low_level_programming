0426b1f6e38cd38891016217ecc1a54832905a2f
YalexM <ya1018mi@gmail.com>
2022-12-30 04:53:41 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ for (a = 0; a <= 99; a++)
 		putchar((a % 10) + '0');
 		putchar(' ');
 		putchar((b / 10) + '0');
-		putchar((b % 1)0 + '0');
+		putchar((b % 10) + '0');
 		if (a == 99 && b == 100)
 			continue;
 		putchar(',');
