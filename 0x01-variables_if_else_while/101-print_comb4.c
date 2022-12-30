266fdde3bdedcfb0dcfb3983e4e68357b16297d8
YalexM <ya1018mi@gmail.com>
2022-12-29 17:08:33 -0800

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - prints combinations of three digits.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a;
+int b;
+int c;
+	for (a = 48; a < 58; a++)
+	{
+	for (b = 48; b < 58; b++)
+	{
+	if (b > a)
+	{
+	for (c = 48; c < 58; c++)
+	{
+	if (c > b)
+	{
+		putchar(a);
+		putchar(b);
+		putchar(c);
+	if (a == 55 && b == 56 && c == 57;)
+	{
+		break
+	}
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+
