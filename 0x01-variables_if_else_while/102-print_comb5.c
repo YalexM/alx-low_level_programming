9fcab7583103f1de217850e142e2ccb46788bd66
YalexM <ya1018mi@gmail.com>
2022-12-30 04:50:35 -0800

    fix task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,24 +9,19 @@ int main(void)
 {
 int a;
 int b;
-	for (a = 0; a < 100; a++)
+for (a = 0; a <= 99; a++)
 	{
-	for (b = 0; b < 100; b++)
+	for (b = a + 1; b <= 100; b++)
 	{
-	if (a > b)
-
-	{
-		putchar(a / 10 + '0');
-		putchar(a % 10 + '0');
+		putchar((a / 10) + '0');
+		putchar((a % 10) + '0');
 		putchar(' ');
-		putchar(b / 10 + '0');
-		putchar(b % 10 + '0');
-		while (a = 100 + b != 9899)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+		putchar((b / 10) + '0');
+		putchar((b % 1)0 + '0');
+		if (a == 99 && b == 100)
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
 	}
 	putchar('\n');
