d48550683b5e6096d84a57fd1e2d0d0b4d5577f6
YalexM <ya1018mi@gmail.com>
2023-01-01 11:02:24 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - function that print all numbers from (0 to 9).
+ * print_numbers - function that prints all numbers from (0 to 9).
  *
  * Return: return void
  */
