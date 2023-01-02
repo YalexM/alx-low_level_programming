53ab1e15a47f07785413c4d6b9b0fa465ac5fed5
YalexM <ya1018mi@gmail.com>
2023-01-02 00:58:49 -0800

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - memory with a constant byte
+ * @s: A pointer to the memory area to be filled
+ * @b: the contant byte for filling
+ * @n: length of buffer
+ * Return: new string.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int a;
+a = 0;
+	while (a < n)
+	{
+		*(s + a) = b;
+		a++;
+		return (s);
+	}
+}
