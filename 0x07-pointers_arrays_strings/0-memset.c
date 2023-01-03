fcaefa8aa33fd7ec0471cc762518c330b4653fcb
YalexM <ya1018mi@gmail.com>
2023-01-03 04:48:52 -0800

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int a;
+int a;
 a = 0;
-	while (a < n)
+	for (a = 0; n > 0; a++)
 	{
-		*(s + a) = b;
-		a++;
-		return (0);
+		*s[a] = b;
+		n--;
+		return (s);
 	}
 }
