b597cb8bae8f3b1375da94e82efb91eadc99d827
YalexM <ya1018mi@gmail.com>
2023-01-03 06:23:44 -0800

    task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints sum of two diagonals of a squere.
+ * @a: pointer of array.
+ * @size: size of array.
+ * Return: Always 0.
+ */
+
+void print_diagsums(int *a, int size)
+{
+int i, sumdiagonla1, sumdiagonal2;
+	for (i = 0; i < size; i++)
+	{
+		sumdiagonla1 += a[i];
+		a += size;
+	}
+	a -= size;
+	for (i = 0; i < size; i++)
+	{
+		sumdiagonal2 += a[i];
+		a -= size;
+	}
+	print("%d, %d\n", sumdiagonla1, sumdiagonal2);
+	return (0);
+}
